f3b8ff95b219f8804b436583f3e0a6aab78b3b5e
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-01-05 10:28:40 +0700

    I'm so dumb because of using bad polyfill lol. I should have used .slice(-1)[0] instead

--- handleImage.js
+++ handleImage.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 
 /* //Sauce: https://futurestud.io/tutorials/download-files-images-with-axios-in-node-js
 async function downloadImage(axios, imageUrl, saveDir = `${__dirname}/images`) {
-    const writer = fs.createWriteStream(Path.resolve(saveDir, imageUrl.split('/').at(-1)))
+    const writer = fs.createWriteStream(Path.resolve(saveDir, imageUrl.split('/').slice(-1)[0]))
     const response = await axios(imageUrl, { responseType: 'stream' })
     response.data.pipe(writer)
     await new Promise(re => setTimeout(re, 100))
@@ -12,14 +12,9 @@ async function downloadImage(axios, imageUrl, saveDir = `${__dirname}/images`) {
         writer.on('error', reject)
     })
 } */
-//Array.prototype.at is a very new feature. Only Node.js 16.6.0+ support it
-//Sauce: https://dev.to/lexlohr/comment/1lcbl
-if (![].at) {
-    Array.prototype.at = function (pos) { return this.slice(pos, pos + 1)[0] }
-}
 async function downloadImage(axios, imageUrl, { saveDir, episodeTitle }) {
     const response = await axios(imageUrl, { responseType: "arraybuffer" })
-    fs.writeFileSync(path.resolve(saveDir, episodeTitle, imageUrl.split('/').at(-1)), response.data)
+    fs.writeFileSync(path.resolve(saveDir, episodeTitle, imageUrl.split('/').slice(-1)[0]), response.data)
 }
 
 module.exports = { downloadImage }
