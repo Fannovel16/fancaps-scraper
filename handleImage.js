b1c2fdd13fcc8526af215305e486c5ee25d6f9ea
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-01-05 10:19:58 +0700

    Add Array.prototype.at prototype

--- handleImage.js
+++ handleImage.js
@@ -12,7 +12,11 @@ async function downloadImage(axios, imageUrl, saveDir = `${__dirname}/images`) {
         writer.on('error', reject)
     })
 } */
-
+//Array.prototype.at is a very new feature. Only Node.js 16.6.0+ support it
+//Sauce: https://dev.to/lexlohr/comment/1lcbl
+if (![].at) {
+    Array.prototype.at = function (pos) { return this.slice(pos, pos + 1)[0] }
+}
 async function downloadImage(axios, imageUrl, { saveDir, episodeTitle }) {
     const response = await axios(imageUrl, { responseType: "arraybuffer" })
     fs.writeFileSync(path.resolve(saveDir, episodeTitle, imageUrl.split('/').at(-1)), response.data)
