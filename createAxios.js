19df61a9674b704e2aee8fa9a17866aed11541da
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-01-07 23:12:13 +0700

    .

--- createAxios.js
+++ createAxios.js
@@ -5,7 +5,7 @@ const { default: axiosRetry } = require('axios-retry')
 module.exports = () => {
     let re = axios.create({
         headers: {
-            "User-Agent": randomUseragent.getRandom(ua => ["Chrome", "Firefox", "Safari"].includes(ua.browserName) && ua.osName === "Windows" && ua.osVersion === "10")
+            //"User-Agent": randomUseragent.getRandom(ua => ["Chrome", "Firefox", "Safari"].includes(ua.browserName) && ua.osName === "Windows" && ua.osVersion === "10")
         }
     })
     axiosRetry(re, { retries: 10, retryDelay: () => 1000 })
