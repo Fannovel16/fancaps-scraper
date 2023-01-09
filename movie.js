31fdff85310a7eff20369ead787f4da6326a332c
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-01-09 16:04:06 +0700

    Remove --skipNLastPage

--- movie.js
+++ movie.js
@@ -19,9 +19,9 @@ async function getMovieData(movieUrl, { skipNLastPages, numOfPromises }) {
             break
         }
         imageUrls2d.push(currImageUrls2d.flat())
-        i++ //?
+        i+= numOfPromises
     }
-    if (skipNLastPages) imageUrls2d = imageUrls2d.slice(0, -skipNLastPages)
+    //if (skipNLastPages) imageUrls2d = imageUrls2d.slice(0, -skipNLastPages)
 
     movieUrl.searchParams.delete("page")
     return {
